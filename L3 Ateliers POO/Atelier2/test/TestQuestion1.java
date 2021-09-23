f4be72c59bff6cd7e71d504dc7e70fd851333361
Félicien <felicien.bertrand@gmail.com>
2021-09-23 10:25:34 +0200

    Mise à jour Ateliers POO 1 et 2

---
+++ L3 Ateliers POO/Atelier2/test/TestQuestion1.java
@@ -0,0 +1,25 @@
+package test;
+
+import util.Personne;
+
+public class TestQuestion1 {
+
+	public static void main(String[] args) {
+		//Création de 3 personnes
+		Personne p1 = new Personne("Test", "Personne", 6, 7, 2000, 8, "RueTest", "88888", "TestVille");
+		Personne p2 = new Personne("Test", "Personne", 3, 4, 2002, 9, "RueTest", "99999", "VilleTest");
+		Personne p3 = new Personne("Test", "Personne", 6, 7, 2000, 8, "RueTest", "88888", "TestVille");
+		
+		//Test plusAgee et plusAgeeQue
+		System.out.println("Test plusAgee : p1 est plus âgée que p2");
+		System.out.println("plusAgee(p1, p2) : " + Personne.plusAgee(p1, p2));
+		System.out.println("plusAgee(p2, p1) : " + Personne.plusAgee(p2, p1));
+		System.out.println("p1.plusAgeeQue(p2) : " + p1.plusAgeeQue(p2));
+		System.out.println("p2.plusAgeeQue(p1) : " + p2.plusAgeeQue(p1));
+		
+		//Test equals
+		System.out.println("Test equals : p1 est identique à p3 (mais pas à p2)");
+		System.out.println("p1.equals(p3) : " + p1.equals(p3));
+		System.out.println("p1.equals(p2) : " + p1.equals(p2));
+	}
+}
